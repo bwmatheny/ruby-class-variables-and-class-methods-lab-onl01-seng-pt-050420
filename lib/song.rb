0a5aecc7307e763701affc56bede0db1a431ed15
Brian Matheny <bwmatheny@gmail.com>
2020-05-19 14:53:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,14 +3,14 @@ class Song
 attr_accessor :name, :artist, :genre
 
 @@count = 0
-@@artist = Array.new
+@@artists = Array.new
 
   def initialize(name, artist, genre)
     @@count += 1
     @name = name
     @artist = artist
     @genre = genre
-    @@artist.push(artist)
+    @@artists.push(artist)
   end
 
 end
